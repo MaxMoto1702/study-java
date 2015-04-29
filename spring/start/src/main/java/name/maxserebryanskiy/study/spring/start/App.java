6431de64c3b1eb9ac71f0d70e7f2b81ced068c1d
Макс Серебрянский <smv@live.ru>
2015-04-29 09:38:56 +0300

    Spring Start complete. http://spring-projects.ru/projects/spring-framework/

--- spring/start/src/main/java/name/maxserebryanskiy/study/spring/start/App.java
+++ spring/start/src/main/java/name/maxserebryanskiy/study/spring/start/App.java
@@ -1,10 +1,26 @@
 package name.maxserebryanskiy.study.spring.start;
 
-/**
- * Hello world!
- */
+import name.maxserebryanskiy.study.spring.start.component.MessagePrinter;
+import name.maxserebryanskiy.study.spring.start.service.MessageService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        printer.printMessage();
+    }
+
+    @Bean
+    MessageService mockMessageService() {
+        return new MessageService() {
+            public String getMessage() {
+                return "Hello World!!!";
+            }
+        };
     }
 }
