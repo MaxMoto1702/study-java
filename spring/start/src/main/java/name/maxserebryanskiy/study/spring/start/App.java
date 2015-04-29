ff3bf1058c8c882677e8905d080f94a9735ef9fc
Макс Серебрянский <smv@live.ru>
2015-04-29 09:37:25 +0300

    Spring Start init

---
+++ spring/start/src/main/java/name/maxserebryanskiy/study/spring/start/App.java
@@ -0,0 +1,10 @@
+package name.maxserebryanskiy.study.spring.start;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
