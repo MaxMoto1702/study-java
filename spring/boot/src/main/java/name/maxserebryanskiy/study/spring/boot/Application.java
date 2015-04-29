b34f9f6f4a928b98c0006b2b4d907185a182f82b
Макс Серебрянский <smv@live.ru>
2015-04-29 13:47:26 +0300

    Spring Boot draft

---
+++ spring/boot/src/main/java/name/maxserebryanskiy/study/spring/boot/Application.java
@@ -0,0 +1,17 @@
+package name.maxserebryanskiy.study.spring.boot;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hello world!
+ */
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+    }
+}
