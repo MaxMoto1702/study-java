5f1d3f57463bd0dd9999b6c36b6b6d9b346d2fec
Макс Серебрянский <smv@live.ru>
2015-04-29 11:44:26 +0300

    Add model and controller

---
+++ spring/rest-service/src/main/java/name/maxserebryanskiy/study/spring/rest/service/Greeting.java
@@ -0,0 +1,23 @@
+package name.maxserebryanskiy.study.spring.rest.service;
+
+/**
+ * Created by m on 29.04.2015.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
