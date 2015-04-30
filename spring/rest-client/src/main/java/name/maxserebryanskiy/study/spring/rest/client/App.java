6425f932077ae81d6404ae8a143cebc9f09e000e
Макс Серебрянский <smv@live.ru>
2015-04-30 15:49:26 +0300

    spring rest-client

---
+++ spring/rest-client/src/main/java/name/maxserebryanskiy/study/spring/rest/client/App.java
@@ -0,0 +1,10 @@
+package name.maxserebryanskiy.study.spring.rest.client;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
