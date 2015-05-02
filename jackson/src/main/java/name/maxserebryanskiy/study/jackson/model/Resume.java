ea7a74f8540d38386002174a044bedef38833a98
Макс Серебрянский <smv@live.ru>
2015-05-02 16:05:20 +0300

    Lite version

--- jackson/src/main/java/name/maxserebryanskiy/study/jackson/model/Resume.java
+++ jackson/src/main/java/name/maxserebryanskiy/study/jackson/model/Resume.java
@@ -5,19 +5,14 @@ package name.maxserebryanskiy.study.jackson.model;
  */
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import name.maxserebryanskiy.study.jackson.domain.Area;
-
-import java.util.Calendar;
-import java.util.List;
-import java.util.Set;
 
 
 public class Resume {
 
-    @JsonProperty("id")
-    private String id;
-    @JsonProperty("alternate_url")
-    private String alternateUrl;
+//    @JsonProperty("id")
+//    private String id;
+//    @JsonProperty("alternate_url")
+//    private String alternateUrl;
 
     @JsonProperty("first_name")
     private String firstName;
@@ -25,61 +20,61 @@ public class Resume {
     private String middleName;
     @JsonProperty("last_name")
     private String lastName;
-    @JsonProperty("gender")
-    private Gender gender;
-    @JsonProperty("birth_date")
-    private Calendar birthDate;
-    @JsonProperty("age")
-    private Integer age;
-    @JsonProperty("contact")
-    private List<Contact> contact;
-    @JsonProperty("area")
-    private Area area;
+    //    @JsonProperty("gender")
+//    private Gender gender;
+//    @JsonProperty("birth_date")
+//    private Calendar birthDate;
+//    @JsonProperty("age")
+//    private Integer age;
+//    @JsonProperty("contact")
+//    private List<Contact> contact;
+//    @JsonProperty("area")
+//    private Area area;
     @JsonProperty("photo")
     private Photo photo;
-    @JsonProperty("citizenship")
-    private List<Area> citizenship;
-    @JsonProperty("site")
-    private List<Site> site;
+//    @JsonProperty("citizenship")
+//    private List<Area> citizenship;
+//    @JsonProperty("site")
+//    private List<Site> site;
 
     @JsonProperty("title")
     private String title;
-    @JsonProperty("specialization")
-    private List<Specialization> specialization;
-    @JsonProperty("experience")
-    private List<Experience> experience;
-    @JsonProperty("salary")
-    private Salary salary;
-    @JsonProperty("resume_locale")
-    private ResumeLocale resumeLocale;
-    @JsonProperty("travel_time")
-    private TravelTime travelTime;
-    @JsonProperty("schedule")
-    private Schedule schedule;
-    @JsonProperty("employment")
-    private Employment employment;
-    @JsonProperty("schedules")
-    private List<Schedule> schedules;
-    @JsonProperty("employments")
-    private List<Employment> employments;
-    @JsonProperty("relocation")
-    private Relocation relocation;
-    @JsonProperty("work_ticket")
-    private List<WorkTicket> workTicket;
+//    @JsonProperty("specialization")
+//    private List<Specialization> specialization;
+//    @JsonProperty("experience")
+//    private List<Experience> experience;
+//    @JsonProperty("salary")
+//    private Salary salary;
+//    @JsonProperty("resume_locale")
+//    private ResumeLocale resumeLocale;
+//    @JsonProperty("travel_time")
+//    private TravelTime travelTime;
+//    @JsonProperty("schedule")
+//    private Schedule schedule;
+//    @JsonProperty("employment")
+//    private Employment employment;
+//    @JsonProperty("schedules")
+//    private List<Schedule> schedules;
+//    @JsonProperty("employments")
+//    private List<Employment> employments;
+//    @JsonProperty("relocation")
+//    private Relocation relocation;
+//    @JsonProperty("work_ticket")
+//    private List<WorkTicket> workTicket;
 
-    @JsonProperty("skill_set")
-    private Set<String> skillSet;
-    @JsonProperty("education")
-    private Education education;
-    @JsonProperty("recommendation")
-    private List recommendation;
-    @JsonProperty("language")
-    private List<Language> language;
-    @JsonProperty("skills")
-    private String skills;
-    @JsonProperty("portfolio")
-    private List portfolio;
+//    @JsonProperty("skill_set")
+//    private Set<String> skillSet;
+//    @JsonProperty("education")
+//    private Education education;
+//    @JsonProperty("recommendation")
+//    private List recommendation;
+//    @JsonProperty("language")
+//    private List<Language> language;
+//    @JsonProperty("skills")
+//    private String skills;
+//    @JsonProperty("portfolio")
+//    private List portfolio;
 
-    @JsonProperty("updated_at")
-    private Calendar updatedAt;
+//    @JsonProperty("updated_at")
+//    private Calendar updatedAt;
 }
